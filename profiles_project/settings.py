6282193956a2614439a4c0bce927ba571e18c163
Samuel <samuel.souto7@gmail.com>
2020-03-22 21:20:12 -0300

    Adicionando django-cors-headers

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -48,9 +48,12 @@ INSTALLED_APPS = [
     'core',
     'product',
     'catalog',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
