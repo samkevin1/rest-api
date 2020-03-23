ae2dac9c50ddc18e7030f32681615c000af15069
André Rebonato <andrerebonato@users.noreply.github.com>
2020-03-22 23:15:32 -0300

    teste

--- user/urls.py
+++ user/urls.py
@@ -4,19 +4,11 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 
-class Response:
-    def __init__(self, success, data):
-        self.success = success
-        self.data = data
-
 router = DefaultRouter()
-router.register(r'', Response(True, views.ListUser))
-
 
 urlpatterns = [
+    path('', views.ListUser.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
     path('create/', views.UserProfileViewSet.as_view()),
     path('me/', views.ManageUserView.as_view(), name='me'),
 ]
-
-urlpatterns += router.urls
