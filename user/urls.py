0202f9c47acd20ab94deb491a4a5074017ff0803
André Rebonato <andrerebonato@users.noreply.github.com>
2020-03-31 23:31:28 -0300

    Finished user urls to test.

--- user/urls.py
+++ user/urls.py
@@ -8,10 +8,11 @@ router = DefaultRouter()
 
 urlpatterns = [
     path('list/', views.get_all),
-    path('disable/list', views.get_all_disabled),
+    path('disabled/list/', views.get_all_disabled),
     path('login/', views.UserLoginApiView.as_view()),
     path('create/', views.create),
-    path('details/<str:pk>/', views.get_by_id),
-    path('update/<str:pk>/', views.update),
-    path('delete/<str:pk>/', views.DeleteUserView.as_view())
+    path('details/<int:pk>/', views.get_by_id),
+    path('update/<int:pk>/', views.update),
+    path('delete/<int:pk>/', views.delete),
+    path('active/<int:pk>/', views.active)
 ]
